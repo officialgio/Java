6a7660c022b12af9c430ddc87325438d12ebce29
Giovanny Hernandez <87216285+officialgio@users.noreply.github.com>
2021-10-20 12:27:23 -0500

    Create ReturningIndexOfAString.java

---
+++ Computing/ReturningIndexOfAString.java
@@ -0,0 +1,14 @@
+// String Demo
+
+public class StringDemo {
+    public static void main(String[] args) {
+
+        String message = "Welcome to Java"; // create string
+        int mLength = message.length(); // 15 (returns length of the string
+        char ch = message.charAt(11); //charAt returns an specified index of a string
+
+        //System.out.print(mLength);
+
+        System.out.print(ch);
+    }
+}
