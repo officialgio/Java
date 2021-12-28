8dec90f891343174944ed2d82dc57bc2845faa22
Giovanny Hernandez <87216285+officialgio@users.noreply.github.com>
2021-12-28 13:34:05 -0600

    Create sum2.java

---
+++ Problems/sum2.java
@@ -0,0 +1,16 @@
+/* Given an array of ints, return the sum of the first 2 elements in the 
+ * array. If the array length is less than 2, just sum up the elements that 
+ * exist, returning 0 if the array is length 0.
+ */
+
+public int sum2(int[] nums) {
+  int sum = 0;
+  
+  if (nums.length == 0) {
+    return sum;
+  }
+  if (nums.length == 1) {
+    return nums[0];
+  }
+  return nums[0] + nums[1];
+}
