834f953aab327121e99a08fef3438dce13f318db
Giovanny Hernandez <87216285+officialgio@users.noreply.github.com>
2021-12-28 16:43:05 -0600

    Create countEvens.java

---
+++ Problems/countEvens.java
@@ -0,0 +1,13 @@
+/* Return the number of even ints in the given array.
+ */
+
+ public int countEvens(int[] nums) {
+    int count = 0;
+    
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 == 0) { // if is even
+          count++;
+    }
+  }
+    return count;
+}
