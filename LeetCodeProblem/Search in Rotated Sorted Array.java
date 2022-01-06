03a735b512f7354a14f18dc4cebe21ff04b0abbc
Giovanny Hernandez <87216285+officialgio@users.noreply.github.com>
2022-01-06 17:37:09 -0600

    Update Search in Rotated Sorted Array.java

--- LeetCodeProblem/Search in Rotated Sorted Array.java
+++ LeetCodeProblem/Search in Rotated Sorted Array.java
@@ -21,6 +21,26 @@ Output: -1
 
 */
 
+//Linear Search Method
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        // linear search 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
+
+
+//Binary Search Method
+
 class Solution {
     public int search(int[] nums, int target) {
         if (nums == null && nums.length == 0) return -1; 
